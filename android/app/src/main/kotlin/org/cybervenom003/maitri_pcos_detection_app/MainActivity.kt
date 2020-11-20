a58a9e1786a9c79643b8e239c05c90417920671d
cyber-venom003 <tech.worm.1728@gmail.com>
2020-11-20 14:28:02 +0530

    Setup basic flutter project with HomePage

---
+++ android/app/src/main/kotlin/org/cybervenom003/maitri_pcos_detection_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.cybervenom003.maitri_pcos_detection_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(Driver());
+}
+
+class Driver extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Color(0xFFCE0C0C),
+                title: Text(
+                    'Maitri',
+                    style: TextStyle(
+                      fontSize: 22.5
+                    ),
+                ),
+                bottom: TabBar(
+                  tabs: [
+                    Tab(text: 'About PCOS', icon: Icon(Icons.info_outline , size: 27.5,),),
+                    Tab(text: 'Test', icon: FaIcon(FontAwesomeIcons.stethoscope , size: 27.5,)),
+                    Tab(text: 'Remedies', icon: FaIcon(FontAwesomeIcons.bookMedical , size: 27.5,)),
+                  ],
+                ),
+              ),
+            )
+        )
+    );
+  }
+}
